2f2f0e5efb13bdb5f27ff47ba2da815a2f2e00c4
Samuel <samwoker112@gmail.com>
2025-11-29 14:26:46 +0300

    Initial commit

---
+++ lib/common/shared/color.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryBrand = Color(0xF48FB1);
+  static const Color secondaryBrand = Color(0x4CAF50);
+  static const Color background = Color(0xFFFFFF);
+  static const Color canvas = Color(0xF5F7FA);
+  static const Color textPrimary = Color(0x1A1A1A);
+  static const Color textSecondary = Color(0x9E9E9E);
+  static const Color alert = Color(0xFF5252);
+}
